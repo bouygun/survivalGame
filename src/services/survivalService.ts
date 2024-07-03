3615a60f71bcf39c2ab51fe6abaa3642e0f13668
bouygun <b.uygun@ebebek.com>
2024-07-03 11:30:48 +0300

    feat: test with ava

--- src/services/survivalService.ts
+++ src/services/survivalService.ts
@@ -1,55 +1,69 @@
-import { survivalServisInput } from '../models/models';
-import { ErrorHandler } from '../utils/errorHandler';
+import { survivalServisInput } from "../models/models";
+import { ErrorHandler } from "../utils/errorHandler";
 
-export class SimulationService {
-    public simulate(parameters: survivalServisInput): string[] {
-        const {resourceDistance, hero, enemies} = parameters
-        const responseArr: string[] = [];
-        try {
-            if(resourceDistance <= 0){
-                throw new ErrorHandler(400, "Resource distance must be greater than zero.")
-            }
-            if (hero.hp <= 0 || hero.attack <= 0) {
-                throw new ErrorHandler(400, "Hero's hp and attack must be greater than zero.");
-            }
-            enemies.forEach(enemy => {
-                if (enemy.enemy.hp <= 0 || enemy.enemy.attack <= 0) {
-                    throw new ErrorHandler(400, "Enemy's hp and attack must be greater than zero.");
-                }
-            });
-            responseArr.push(`Hero started journey with ${hero.hp} HP!`);
-        
-        enemies.sort((a, b) => a.position - b.position)
-
-        for (const enemyPosition of enemies) {
-            if (enemyPosition.position > resourceDistance) break;
-
-            const { enemy, position } = enemyPosition;
-
-            while (hero.hp > 0 && enemy.hp > 0) {
-                hero.hp -= enemy.attack;
-                enemy.hp -= hero.attack;
-            }
-            if (hero.hp > 0) {
-                responseArr.push(`Hero defeated ${enemyPosition.type} with ${hero.hp} HP remaining`);
-            } else {
-                responseArr.push(`Hero was defeated by ${enemyPosition.type} at position ${position}`);
-                responseArr.push(`Hero is Dead!! Last seen at position ${position}!!`);
-                return responseArr;
-            }
+export class SurvivalSimulation {
+  public simulate(parameters: survivalServisInput): string[] {
+    const { resourceDistance, hero, enemies } = parameters;
+    const responseArr: string[] = [];
+    try {
+      if (resourceDistance <= 0) {
+        throw new ErrorHandler(
+          400,
+          "Resource distance must be greater than zero."
+        );
+      }
+      if (hero.hp <= 0 || hero.attack <= 0) {
+        throw new ErrorHandler(
+          400,
+          "Hero's hp and attack must be greater than zero."
+        );
+      }
+      enemies.forEach((enemy) => {
+        if (enemy.enemy.hp <= 0 || enemy.enemy.attack <= 0) {
+          throw new ErrorHandler(
+            400,
+            "Enemy's hp and attack must be greater than zero."
+          );
         }
+      });
+      responseArr.push(`Hero started journey with ${hero.hp} HP!`);
 
+      enemies.sort((a, b) => a.position - b.position);
+
+      for (const enemyPosition of enemies) {
+        if (enemyPosition.position > resourceDistance) break;
+
+        const { enemy, position } = enemyPosition;
+
+        while (hero.hp > 0 && enemy.hp > 0) {
+          hero.hp -= enemy.attack;
+          enemy.hp -= hero.attack;
+        }
         if (hero.hp > 0) {
-            responseArr.push(`Hero Survived!`);
+          responseArr.push(
+            `Hero defeated ${enemyPosition.type} with ${hero.hp} HP remaining`
+          );
+        } else {
+          responseArr.push(
+            `Hero was defeated by ${enemyPosition.type} at position ${position}`
+          );
+          responseArr.push(
+            `Hero is Dead!! Last seen at position ${position}!!`
+          );
+          return responseArr;
         }
+      }
 
-        } catch (error) {
-            if (error instanceof ErrorHandler) {
-                throw error;
-            } else {
-                throw new ErrorHandler(500, "An error occurred during the simulation.");
-            }
-        }
-        return responseArr
+      if (hero.hp > 0) {
+        responseArr.push(`Hero Survived!`);
+      }
+    } catch (error) {
+      if (error instanceof ErrorHandler) {
+        throw error;
+      } else {
+        throw new ErrorHandler(500, "An error occurred during the simulation.");
+      }
     }
+    return responseArr;
+  }
 }
---
+++ tests/survivalService.test.ts
@@ -0,0 +1,128 @@
+import test from "ava";
+import { SurvivalSimulation } from "../src/services/survivalService";
+import { Hero, Enemy } from "../src/models/types";
+import { ErrorHandler } from "../src/utils/errorHandler";
+
+test.serial("If hero survives", async (t) => {
+  const simulationService = new SurvivalSimulation();
+
+  const hero: Hero = { hp: 1000, attack: 10 };
+  const enemies: Enemy[] = [
+    { enemy: { hp: 50, attack: 2 }, position: 276, type: "Bug" },
+    { enemy: { hp: 50, attack: 2 }, position: 489, type: "Bug" },
+    { enemy: { hp: 100, attack: 15 }, position: 1527, type: "Lion" },
+    { enemy: { hp: 300, attack: 7 }, position: 1681, type: "Zombie" },
+    { enemy: { hp: 100, attack: 15 }, position: 2865, type: "Lion" },
+    { enemy: { hp: 300, attack: 7 }, position: 3523, type: "Zombie" },
+  ];
+  const resourceDistance = 5000;
+
+  const result = simulationService.simulate({
+    hero,
+    enemies,
+    resourceDistance,
+  });
+
+  const isHeroSurvived = result.some((r) => r.includes("Hero Survived!"));
+  t.true(isHeroSurvived);
+  // like output sample
+  t.deepEqual(result, [
+    "Hero started journey with 1000 HP!",
+    "Hero defeated Bug with 990 HP remaining",
+    "Hero defeated Bug with 980 HP remaining",
+    "Hero defeated Lion with 830 HP remaining",
+    "Hero defeated Zombie with 620 HP remaining",
+    "Hero defeated Lion with 470 HP remaining",
+    "Hero defeated Zombie with 260 HP remaining",
+    "Hero Survived!",
+  ]);
+});
+
+test.serial("If hero lost and dies", async (t) => {
+  const simulationService = new SurvivalSimulation();
+
+  const hero: Hero = { hp: 500, attack: 9 };
+  const enemies: Enemy[] = [
+    { enemy: { hp: 400, attack: 8 }, position: 274, type: "Mutant" },
+    { enemy: { hp: 75, attack: 10 }, position: 486, type: "ZombieDog" },
+    { enemy: { hp: 75, attack: 10 }, position: 1897, type: "ZombieDog" },
+    { enemy: { hp: 300, attack: 7 }, position: 1687, type: "Zombie" },
+    { enemy: { hp: 400, attack: 8 }, position: 5332, type: "Mutant" },
+  ];
+  const resourceDistance = 7500;
+
+  const result = simulationService.simulate({
+    hero,
+    enemies,
+    resourceDistance,
+  });
+
+  const isHeroDied = result.some((r) => r.includes("Hero Survived!"));
+  t.false(isHeroDied);
+  // like output sample
+
+  t.deepEqual(result, [
+    "Hero started journey with 500 HP!",
+    "Hero defeated Mutant with 140 HP remaining",
+    "Hero defeated ZombieDog with 50 HP remaining",
+    "Hero was defeated by Zombie at position 1687",
+    "Hero is Dead!! Last seen at position 1687!!",
+  ]);
+});
+
+test.serial("Invalid resourceDistance in request", async (t) => {
+  const simulationService = new SurvivalSimulation();
+
+  const hero: Hero = { hp: 1000, attack: 10 };
+  const enemies: Enemy[] = [
+    { enemy: { hp: 50, attack: 2 }, position: 276, type: "Bug" },
+  ];
+  const resourceDistance = 0;
+
+  const error = t.throws(
+    () => {
+      simulationService.simulate({ hero, enemies, resourceDistance });
+    },
+    { instanceOf: ErrorHandler }
+  );
+
+  t.is(error?.message, "Resource distance must be greater than zero.");
+});
+
+test.serial("Invalid hero stats", async (t) => {
+  const simulationService = new SurvivalSimulation();
+
+  const hero: Hero = { hp: 0, attack: 10 };
+  const enemies: Enemy[] = [
+    { enemy: { hp: 50, attack: 2 }, position: 276, type: "Bug" },
+  ];
+  const resourceDistance = 5000;
+
+  const error = t.throws(
+    () => {
+      simulationService.simulate({ hero, enemies, resourceDistance });
+    },
+    { instanceOf: ErrorHandler }
+  );
+
+  t.is(error?.message, "Hero's hp and attack must be greater than zero.");
+});
+
+test.serial("Invalid enemy stats", async (t) => {
+  const simulationService = new SurvivalSimulation();
+
+  const hero: Hero = { hp: 1000, attack: 10 };
+  const enemies: Enemy[] = [
+    { enemy: { hp: 0, attack: 2 }, position: 276, type: "Bug" },
+  ];
+  const resourceDistance = 5000;
+
+  const error = t.throws(
+    () => {
+      simulationService.simulate({ hero, enemies, resourceDistance });
+    },
+    { instanceOf: ErrorHandler }
+  );
+
+  t.is(error?.message, "Enemy's hp and attack must be greater than zero.");
+});
